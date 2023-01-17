01390661cb24621af8c4bc73df894b4f3be6b1b8
Pooja R <pooja.ramesh@imaginelearning.com>
2023-01-17 17:30:30 +0530

    Dark Mode Button added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import DarkLightMode from "./components/DarkLightMode";
 
 function App() {
   return (
     <>
-      <Navbar title="Pooja" />
-
+      <div className="d-flex justify-content-sm-between align-items-center px-3">
+        <Navbar title="TextUtilizer" />
+        <DarkLightMode />
+      </div>
       <div className="container mt-5">
-        <TextForm heading="Enter your text below to convert into Uppercase: "/>
+        <TextForm heading="Enter your text below to convert into Uppercase: " />
       </div>
     </>
   );
---
+++ src/components/DarkLightMode.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+
+export default function DarkLightMode() {
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
+  return (
+    <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={80} />
+  );
+}
